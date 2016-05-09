a1ef2dcb48e343cd340df18bf49fc831dd10660b
Wes Mason <wes@1stvamp.org>
2016-05-09 18:48:36 +0100

    Add working install handler

--- reactive/npm_offline_registry.py
+++ reactive/npm_offline_registry.py
@@ -1,17 +1,14 @@
-from charms.reactive import when, when_not, set_state
+from charms.nodejs import npm, node_dist_dir
+from charms.reactive import when, set_state
 
 
-@when_not('npm-offline-registry.installed')
+@when('nodejs.available', 'config.version.changed')
 def install_npm_offline_registry():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
+    npm('install', 'npm-offline-registry@{}'.format(hookenv.config('version')))
     set_state('npm-offline-registry.installed')
+
+
+@when('npm-offline-registry.installed')
+def start_npm_offline_registry():
+    # npm-offline-registry
+    pass
