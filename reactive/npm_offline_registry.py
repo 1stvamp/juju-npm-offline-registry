476066f5697cd7b1abfb997fb63885e0f0c0ff2a
Wes Mason <wes@1stvamp.org>
2016-05-10 14:21:30 +0100

    Fix npm() call for installing app, and only do it if version is supplied

--- reactive/npm_offline_registry.py
+++ reactive/npm_offline_registry.py
@@ -1,11 +1,14 @@
+from charmhelpers.core import hookenv
 from charms.layer.nodejs import npm, node_dist_dir
 from charms.reactive import when, set_state
 
 
-@when('nodejs.available', 'config.version.changed')
+@when('nodejs.available', 'config.changed.version')
 def install_npm_offline_registry():
-    npm('install', 'npm-offline-registry@{}'.format(hookenv.config('version')))
-    set_state('npm-offline-registry.installed')
+    version = hookenv.config('version')
+    if version:
+        npm('install npm-offline-registry@{}'.format(version))
+        set_state('npm-offline-registry.installed')
 
 
 @when('npm-offline-registry.installed')
