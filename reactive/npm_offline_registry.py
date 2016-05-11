729a6c6666c3e8f8468401d5d23865e4aa70fa22
Wes Mason <wes@1stvamp.org>
2016-05-11 21:54:55 +0100

    Add contextmanager for maintenance statuses and ensure cache dir is created with correct perms

--- reactive/npm_offline_registry.py
+++ reactive/npm_offline_registry.py
@@ -1,4 +1,7 @@
+from contextlib import contextmanager
+from os import makedirs
 from os.path import join
+from shutil import chown
 
 from charmhelpers.core import hookenv
 from charmhelpers.core.host import adduser, restart_on_change, user_exists
@@ -10,9 +13,25 @@ USER = 'npm-offline-registry'
 UPSTART_PATH = '/etc/init/npm-offline-registry.conf'
 
 
-def ensure_user_exists():
+def maintenance_status(msg):
+    try:
+        hookenv.status_set('maintenance', msg)
+        yield
+    finally:
+        hookenv.status_unset('maintenance')
+
+
+def get_user():
     if not user_exists(USER):
         adduser(USER, shell='/bin/false', system_user=True)
+    return USER
+
+
+def get_cache(base_path, user):
+    cache_path = join(base_path, 'cache-data')
+    makedirs(cache_path, exist_ok=True)
+    chown(cache_path, user=user)
+    return cache_path
 
 
 def get_bin_path():
@@ -24,27 +43,26 @@ def install():
     version = hookenv.config('version')
     if version:
         pkg = 'npm-offline-registry@{}'.format(version)
-        hookenv.status_set('maintenance', 'Installing {} with NPM'.format(pkg))
-
-        npm('install {}'.format(pkg))
-        set_state('npm-offline-registry.installed')
+        with maintenance_status('Installing {} with NPM'.format(pkg)):
+            npm('install {}'.format(pkg))
+            set_state('npm-offline-registry.installed')
 
 
 @when('config.changed', 'npm-offline-registry.installed')
 @restart_on_change({UPSTART_PATH: ['npm-offline-registry']}, stopstart=True)
 def configure():
-    ensure_user_exists()
     dist_dir = node_dist_dir()
+    user = get_user()
 
-    hookenv.status_set('maintenance', 'Generating upstart configuration')
-    config_ctx = hookenv.config()
-    config_ctx['user'] = USER
-    config_ctx['working_dir'] = dist_dir
-    config_ctx['bin_path'] = get_bin_path()
-    config_ctx['npm_cache_path'] = join(dist_dir, 'cache-data')
+    with maintenance_status('Generating upstart configuration'):
+        config_ctx = hookenv.config()
+        config_ctx['working_dir'] = dist_dir
+        config_ctx['user'] = user
+        config_ctx['npm_cache_path'] = get_cache(dist_dir, user)
+        config_ctx['bin_path'] = get_bin_path()
 
-    render(source='npm-offline-registry_upstart.j2',
-           target=UPSTART_PATH,
-           owner='root',
-           perms=0o744,
-           context=config_ctx)
+        render(source='npm-offline-registry_upstart.j2',
+               target=UPSTART_PATH,
+               owner='root',
+               perms=0o744,
+               context=config_ctx)
