0171f4556b7234c881a66e4f55e10a012789b354
Wes Mason <wes@1stvamp.org>
2016-05-16 21:48:31 +0100

    Add the ability to install npm-offline-registry from an SCM repo

--- reactive/npm_offline_registry.py
+++ reactive/npm_offline_registry.py
@@ -3,6 +3,7 @@ from os import makedirs
 from os.path import join
 from shutil import chown
 from subprocess import check_call
+from tempfile import NamedTemporaryFile
 
 from charmhelpers import fetch
 from charmhelpers.core import hookenv
@@ -20,6 +21,8 @@ UPSTART_PATH = '/etc/init/npm-offline-registry.conf'
 
 @contextmanager
 def maintenance_status(begin, end):
+    """ContextManager to easilly wrap an action in maintenance status messages.
+    """
     try:
         hookenv.status_set('maintenance', begin)
         yield
@@ -28,12 +31,17 @@ def maintenance_status(begin, end):
 
 
 def get_user():
+    """Helper to ensure our user exists and return the username.
+    """
     if not user_exists(USER):
         adduser(USER, shell='/bin/false', system_user=True)
     return USER
 
 
 def get_cache(base_path, user):
+    """Helper that ensures a cache data directory exists in our app dir, with
+    correct permissions and returns the path.
+    """
     cache_path = join(base_path, 'cache-data')
     makedirs(cache_path, exist_ok=True)
     chown(cache_path, user=user)
@@ -41,10 +49,18 @@ def get_cache(base_path, user):
 
 
 def get_bin_path(base_path):
+    """Helper that returns the correct path to npm-offline-registry,
+    regardless of if it was installed with NPM or from a repository.
+    """
     return join(base_path, 'node_modules/.bin/npm-offline-registry')
 
 
 def get_local_registry_or_host(uri=False):
+    """Helper to give us either the local_registry config, or use the NGinX
+    host config as backup.
+    If ``uri`` is `True` the returned value will include the HTTP/HTTPS URI
+    prefix.
+    """
     local_cache = hookenv.config('local_cache')
 
     if local_cache:
@@ -61,7 +77,9 @@ def get_local_registry_or_host(uri=False):
 
 
 def is_systemd():
-     return check_call('ps -p1 | grep -q systemd', shell=True)
+    """Helper that returns `True` if the PID1 init system is systemd.
+    """
+    return check_call('ps -p1 | grep -q systemd', shell=True)
 
 
 @when('nodejs.available', 'config.changed.version')
@@ -72,11 +90,60 @@ def install():
         # upstream registry, just in case let's always install it
         fetch.apt_install(fetch.filter_installed_packages(['wget']))
 
-        pkg = 'npm-offline-registry@{}'.format(version)
-        with maintenance_status('Installing {} with NPM'.format(pkg),
-                                '{} installed'.format(pkg)):
-            npm('install {}'.format(pkg))
+        repo = hookenv.config('repo')
+        if repo:
+            install_from_repo(repo, version)
+        else:
+            install_with_npm(version)
+
+
+def install_with_npm(version):
+    pkg = 'npm-offline-registry@{}'.format(version)
+
+    with maintenance_status('Installing {} with NPM'.format(pkg),
+                            '{} installed'.format(pkg)):
+        npm('install {}'.format(pkg))
+        set_state('npm-offline-registry.installed')
+
+
+def install_from_repo(repo, version):
+    pkg = 'npm-offline-registry@{}'.format(version)
+    dist_dir = node_dist_dir()
+    repo_types = ('git', 'hg', 'svn')
+
+    with maintenance_status('Installing {} from {}'.format(pkg, repo),
+                            '{} installed'.format(pkg)):
+        repo_type = hookenv.config('repo_type').lower()
+
+        if repo_type in repo_types:
+            # We use the excellent peru to pull in builds from SCM repos
+            # but it requires a pregenerated YAML file on disk
+            with NamedTemporaryFile() as f:
+                render(source='npm-offline-registry_peru.yaml.j2',
+                    target=f.name,
+                    context={
+                        'url': repo,
+                        'module': repo_type,
+                        'revision': version,
+                    })
+
+                check_call(['sudo', 'su', '-s', '/bin/sh', '-', get_user(),
+                            '-c',
+                            ' '.join(('peru',
+                                      '--file={}'.format(f.name),
+                                      '--sync-dir={}'.format(dist_dir),
+                                      'sync'))])
+
+            # If the repo did not bundle the Node.js dependencies for
+            # npm-offline-registry, then let's try to install them with NPM
+            if not exists(join(dist_dir, 'node_modules')):
+                npm('install')
+
             set_state('npm-offline-registry.installed')
+        else:
+            raise ValueError('Unknown repo_type "{}",not one of {}'.format(
+                             repo_type, repo_types))
+
 
 
 @when('config.changed', 'npm-offline-registry.installed')
@@ -88,6 +155,7 @@ stopstart=True)
 def configure():
     dist_dir = node_dist_dir()
     user = get_user()
+
     if is_systemd():
         conf_path = SYSTEMD_PATH
         template_type = 'systemd'
@@ -128,8 +196,10 @@ def setup_nagios(nagios):
 
 @when('nginx.available')
 def configure_nginx():
-    config_ctx = {
-        'server_name': get_local_registry_or_host(),
-        'cache_dir': get_cache(node_dist_dir(), get_user()),
-    }
-    configure_site('npm-offline-rgistry', 'vhost.conf.j2', **config_ctx)
+    with maintenance_status('Generating NGinX configuration',
+                            'NGinX configuration generated'):
+        config_ctx = {
+            'server_name': get_local_registry_or_host(),
+            'cache_dir': get_cache(node_dist_dir(), get_user()),
+        }
+        configure_site('npm-offline-rgistry', 'vhost.conf.j2', **config_ctx)
